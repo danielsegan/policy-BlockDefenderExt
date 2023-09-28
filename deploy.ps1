29174128e71d05e0573daf74eeaf0bc327cc2ec4
Daniel Segan <dsegan@me.com>
2023-09-28 19:14:08 -0400

    inital commit

---
+++ deploy.ps1
@@ -0,0 +1,18 @@
+$definition = New-AzPolicyDefinition `
+   -Name "not-allowed-vmextension-windows" `
+   -DisplayName "Not allowed VM Extensions" `
+   -description "This policy governs which VM extensions that are explicitly denied."   `
+   -Policy '/Users/dsegan/src/securitas-policy/rules.json' `
+   -Parameter '/Users/dsegan/src/securitas-policy/parameters.json'
+
+$scope = "/subscriptions/f3482bdc-db98-4c56-b961-f7fb0e8eef3f/resourceGroups/myResourceGroup"
+$assignment = New-AzPolicyAssignment `
+   -Name "not-allowed-vmextension-windows" `
+   -Scope $scope `
+   -PolicyDefinition $definition `
+   -PolicyParameter '{
+    "notAllowedExtensions": {
+        "value": ["MDC.Windows","MDE.Windows"]
+    }
+}'
+$assignment
